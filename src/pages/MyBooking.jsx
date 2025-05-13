d5d415a315212334fec89d05a190782240982128
Gnanaprahaasam-SR <sr.gnanaprahaasam@t-xnt.com>
2025-05-13 21:14:42 +0530

    two changes

--- src/pages/MyBooking.jsx
+++ src/pages/MyBooking.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import "../App.css";
-import Prasanam from "../assets/prasanam2.png";
+import Prasanam from "../assets/Prasanam2.png";
 import Jathagam from "../assets/jathagam2.png";
 import Vastu from "../assets/vastu.png";
 import Homam from "../assets/homam.png";
